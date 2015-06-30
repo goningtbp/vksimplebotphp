f6e024deaf64d01020b154518fb5e05dd9ea1b9e
underwear <thisfil@ya.ru>
2015-06-30 20:11:56 +0300

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config['sex'] = 'male';
-$config['name'] = 'Александра';
-$config['repost_time'] = 1; // Количество часов между случайными репостами
-$config['vk_token'] = 'bd07a7f9425b1e57525f8c54d1956217e2ca1c5001c7fc30a8faa3d021def676e36a0dae102688b0730e6';
-?>
